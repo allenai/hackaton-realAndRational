ad51162ea553e78693536334e4238c7dd9a34dda
Eric Gribkoff <eagribko@cs.washington.edu>
2014-08-07 17:12:47 -0700

    improved solver

--- solver.py
+++ solver.py
@@ -6,24 +6,26 @@ import numpy as np
 # (a,b,c) -> (a:{{b,c}}), (b:{{a,c}}), (c:{{a,b}})
 # (b,c,d) -> (b:{{c,d}}), (c:{{b,d}}), (d:{{b,c}})
 def solver(sent_tuples):
-    stats = []
-    outs = [enum(x) for x in sent_tuples]
+    stats = {}
+    #outs = [enum(x) for x in sent_tuples]
     count = 1
-    for combination in itertools.product(*outs):
+    for combination in itertools.product(*sent_tuples):
+        print combination
         if count % 1000 == 0:
             print count
         count = count+1
-        d = {}
         G = nx.DiGraph()
         for key,val in combination:
             for v in val:
                 G.add_edge(key,v)
-
         try:
             k = nx.simple_cycles(G).next()
-            #print combination
         except StopIteration:
-            stats.append(combination)
+            for c in combination:
+                if c in stats:
+                    stats[c] = stats[c] + 1
+                else:
+                    stats[c] = 1
     return stats
 
 def enum(sent_tuple):
@@ -32,10 +34,6 @@ def enum(sent_tuple):
         out.append((sent_tuple[i], set(sent_tuple[0:i]).union(set(sent_tuple[i+1:]))))
     return out
 
-vars = 'abcde'
-k = [x for x in itertools.permutations(vars,3)]
+print solver([[("a",frozenset({"b","c"})),("c",frozenset({"b","a"}))],[("c",frozenset({"d","a"}))]])
 
-np.random.shuffle(k)
-print k[:11]
-print len(solver(k[:11]))
 
